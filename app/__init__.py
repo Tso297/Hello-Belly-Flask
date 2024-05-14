7c3ee33ef118d78599f639480d3fbd6c0ea1f43a
Travis <you@example.com>
2024-05-14 17:59:17 -0400

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_moment import Moment
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+
+CORS(app)
+
+
+from . import models
+
+from .api import api
+
+app.register_blueprint(api)
+moment = Moment(app)
